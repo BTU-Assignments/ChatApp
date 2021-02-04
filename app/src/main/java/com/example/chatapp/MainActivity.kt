9e9991a1e0f5412697f7edd4643f8622bdd1d0cd
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2021-02-04 22:53:20 +0400

    Cool

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appConfig = AppBarConfiguration(setOf(
+            R.id.homeFragment, R.id.registerFragment, R.id.loginFragment, R.id.chatFragment
+        ))
+
+        setupActionBarWithNavController(navController, appConfig)
+        navView.setupWithNavController(navController)
+
+
     }
 }
--- app/src/main/java/com/fragments/HomeFragment.kt
+++ app/src/main/java/com/fragments/HomeFragment.kt
@@ -1,7 +1,35 @@
 package com.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.chatapp.R
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private lateinit var navController : NavController
+    private lateinit var button: Button
+    private lateinit var button2: Button
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button = view.findViewById(R.id.home_registerButton)
+        button2 = view.findViewById(R.id.home_loginButton)
+
+        navController = Navigation.findNavController(view)
+
+
+       button.setOnClickListener {
+
+        }
+
+
+
+
+    }
 }
